9c077627c29c63a2081240de1738c505280ae393
github username <github email address>
2018-05-04 19:20:16 -0400

    formatted wardrobe show view

--- app/controllers/wardrobes_controller.rb
+++ app/controllers/wardrobes_controller.rb
@@ -12,11 +12,16 @@ class WardrobesController < ApplicationController
     erb :'/wardrobes/new'
   end
 
+  get '/wardrobes/:slug' do
+    @wardrobe = Wardrobe.find_by_slug(params[:slug])
+
+    erb :'/wardrobes/show'
+  end
+
   post '/wardrobes/new' do
 
     wardrobe = Wardrobe.create(name: params[:name])
     current_user.wardrobes << wardrobe
-
 
     params[:category][:name].each do |category_name|
       if category_name != ""
@@ -27,6 +32,18 @@ class WardrobesController < ApplicationController
     end
     redirect "/wardrobes"
   end
+
+  delete '/wardrobes/:id/delete' do
+    wardrobe = Wardrobe.find(params[:id])
+    if !logged_in?
+      redirect '/'
+    elsif logged_in? && wardrobe.user_id == current_user.id
+      wardrobe = Wardrobe.find(params[:id])
+      wardrobe.delete
+      redirect '/wardrobes'
+    end
+  end
+
 
 
 end
