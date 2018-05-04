aeac35ba8b62d1cad65239e51690c7fc20eb0fb7
github username <github email address>
2018-05-04 13:43:10 -0400

    coded the create wardrobe table: name, season, user_id

--- db/migrate/20180504173640_create_wardrobes.rb
+++ db/migrate/20180504173640_create_wardrobes.rb
@@ -1,4 +1,9 @@
 class CreateWardrobes < ActiveRecord::Migration[5.2]
   def change
+    create_table :wardrobes do |t|
+      t.string :name
+      t.string :season
+      t.integer :user_id
+    end
   end
 end
