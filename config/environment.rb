7f90b4a164b0198d94d01e963d3301dfec4e8e64
github username <github email address>
2018-05-04 12:53:54 -0400

    reorganized code in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,9 @@
 require 'bundler'
-require_all 'app'
-
 Bundler.require
-
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/development.sqlite"
 )
+
+require_all 'app'
