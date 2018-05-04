ed875eecd8225102be78a14b52de6fc91b205906
github username <github email address>
2018-05-04 13:08:19 -0400

    used rackflash-3 in application_controller class, added configure block: set views, enabled sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < Sinatra::Base
-  
+  use Rack::Flash
+
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret_password"
+  end
 
 
 end
